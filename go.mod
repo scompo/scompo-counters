48c5b55b4c608f7513801c8e6428e5d96c6c0f3a
Mauro Scomparin <scompo@gmail.com>
2020-07-30 03:14:27 +0200

    nodejs?

--- go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/scompo/scompo-counters
-
-go 1.14
-
-require (
-	github.com/duo-labs/webauthn v0.0.0-20200714211715-1daaee874e43
-	github.com/duo-labs/webauthn.io v0.0.0-20200717012147-67203c5c36a7
-	github.com/gorilla/mux v1.7.4
-)
---
+++ index.js
@@ -0,0 +1,126 @@
+const SimpleWebAuthnServer = require('@simplewebauthn/server')
+const express = require('express')
+const https = require('https')
+const fs = require('fs')
+
+const username = 'testUsername'
+const rpID = 'localhost'
+const origin = `https://${rpID}`
+
+const host = '0.0.0.0'
+const port = 443
+
+const memoryDB = {
+  [username]: {
+    id: username,
+    username: `user@${rpID}`,
+    devices: []
+  }
+}
+
+const app = express()
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+app.get('/webauthn/register/start', async (req, res) => {
+  const challenge = 'aNewUniqueChallengeEveryRegistration' // TODO implment it correctly
+  console.log(memoryDB)
+  memoryDB[username].currentChallenge = challenge
+
+  res.json(
+    SimpleWebAuthnServer.generateAttestationOptions({
+      serviceName: 'scompo-counters',
+      rpID: rpID,
+      challenge: challenge,
+      userID: username,
+      userName: username,
+      timeout: 60 * 1000,
+      attestationType: 'direct',
+      excludedCredentialsIDs: memoryDB[username].devices.map(x => x.credentialID),
+      authenticatiorSelection: {
+        userVerification: 'preferred',
+        requireResidentKey: false
+      }
+    }))
+})
+
+app.post('/webauthn/register/end', async (req, res) => {
+  const body = req.body
+  const expectedChallenge = memoryDB[username].currentChallenge
+  SimpleWebAuthnServer.verifyAttestationResponse({
+    credential: body,
+    expectedChallenge: expectedChallenge,
+    expectedOrigin: origin,
+    expectedRPID: rpID
+  })
+    .then(verification => {
+      const { verified, authenticatorInfo } = verification
+      if (verified) {
+        const { base64PublicKey, base64CredentialID, counter } = authenticatorInfo
+        const existingDevice = memoryDB[username].devices.find(d => d.credentialID === base64CredentialID)
+        if (!existingDevice) {
+          memoryDB[username].devices.push({
+            publicKey: base64PublicKey,
+            credentialID: base64CredentialID,
+            counter: counter
+          })
+          console.log('after registration', memoryDB)
+        }
+      }
+      res.json({ verified })
+    })
+    .catch(e => {
+      console.error(e)
+      res.status(400).json(e)
+    })
+})
+
+app.get('/webauthn/login/start', async (req, res) => {
+  const challenge = 'aNewUniqueChallengeEveryAttestation' // TODO implment it correctly
+  memoryDB[username].currentChallenge = challenge
+  console.log('login start:', memoryDB)
+
+  res.json(
+    SimpleWebAuthnServer.generateAssertionOptions({
+      challenge: challenge,
+      timeout: 60 * 1000,
+      allowedCredentialIDs: memoryDB[username].devices.map(x => x.credentialID),
+      userVerification: 'preferred'
+    }))
+})
+
+app.post('/webauthn/login/end', async (req, res) => {
+  const body = req.body
+  const expectedChallenge = memoryDB[username].currentChallenge
+  const dbAuthenticator = memoryDB[username].devices.find(x => x.credentialID === body.id)
+  if (!dbAuthenticator) {
+    throw new Error('Could not find authenticator matching', body.id)
+  }
+  try {
+    const verification = SimpleWebAuthnServer.verifyAssertionResponse({
+      credential: body,
+      expectedChallenge: expectedChallenge,
+      expectedOrigin: origin,
+      expectedRPID: rpID,
+      authenticator: dbAuthenticator
+    })
+    const { verified, authenticatorInfo } = verification
+    if (verified) {
+      dbAuthenticator.counter = authenticatorInfo.counter
+    }
+    res.json({ verified })
+  } catch (e) {
+    console.error(e)
+    res.status(400).json(e)
+  }
+})
+
+const server = https.createServer({
+  key: fs.readFileSync(`./${rpID}.key`),
+  cert: fs.readFileSync(`./${rpID}.crt`)
+}, app)
+
+server.listen(port, host, () => {
+  console.log(`Server started on https://${host}:${port}`)
+})
